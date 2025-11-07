e4789d3a1158a24e0ca27e1632c518cf41dbc250
Andy <apoloandy66@gmail.com>
2025-11-07 07:47:44 -0500

    commit pdf

--- proyecto_base/notify_service_app/views.py
+++ proyecto_base/notify_service_app/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Notification
 
-# Create your views here.
